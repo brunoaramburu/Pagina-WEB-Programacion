80d7e4107088847442f53daf8f02a83f886d8182
macuser <joaquinpeirone@hotmail.com>
2021-11-19 19:52:27 -0300

    commit final

---
+++ backend/api/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from api.models import Productos
+
+class ProductosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Productos
+        fields = "__all__"
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "password"]
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
+
+class MeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = "__all__"
